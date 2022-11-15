94a47adce93e12a4e0fe748604e6c5ddd0f92c6e
Felipe Weiduschadt de Carvalho <felipe.carvalhofln@gmail.com>
2022-11-15 13:48:34 -0300

    chore: created file default routes

---
+++ src/routes/index.tsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes } from 'react-router-dom';
+
+export function AppRoutes() {
+  return (
+    <BrowserRouter>
+        <Routes>
+          <div>Route</div>
+        </Routes>
+    </BrowserRouter>
+  );
+}
