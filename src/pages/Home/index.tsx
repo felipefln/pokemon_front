8b6644c42c86254e83cf324fb36f45099bb14049
Felipe Weiduschadt de Carvalho <felipe.carvalhofln@gmail.com>
2022-11-15 18:54:28 -0300

    feat: created footer component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer'
 import styles from './styles.module.scss';
 
 export function Home() {
   return (
     <>
       <Header />
+      <Footer />
     </>
   );
 }
